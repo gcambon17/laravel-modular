c250d147b0822f8a21b11f650266d92fc4092491
gcambon <gcambon@mtroisg.fr>
2018-10-29 00:36:03 +0100

    Fix migration class name

--- database/migrations/2018_04_01_135256_module.php
+++ database/migrations/2018_04_01_135256_module.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModuleMigration extends Migration
+class Module extends Migration
 {
     /**
      * Run the migrations.
